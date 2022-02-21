d0c30e71188f3df979a81cf5e4eead946ee54682
Bo Hsiang <bottxiang@gmail.com>
2022-02-21 22:19:56 +0800

    add live reload .air.toml

--- test-gin/main.go
+++ test-gin/main.go
@@ -1,17 +1,81 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
 
+	router.GET("/index", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"index": c.FullPath()})
+	})
+
+	router.GET("/redirect", func(c *gin.Context) {
+		c.Redirect(http.StatusMovedPermanently, "/index")
+	})
+
+	router.GET("/goindex", func(c *gin.Context) {
+		c.Request.URL.Path = "/index"
+		router.HandleContext(c)
+	})
+
+	// 匹配 /user/geektutu
+	router.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(http.StatusOK, "Hello %s", name)
+	})
+
+	// 匹配users?name=xxx&role=xxx，role可选
+	router.GET("/users", func(c *gin.Context) {
+		name := c.Query("name")
+		role := c.DefaultQuery("role", "teacher")
+		c.String(http.StatusOK, "%s is a %s", name, role)
+	})
+
+	// POST
+	router.POST("/form", func(c *gin.Context) {
+		username := c.PostForm("username")
+		password := c.DefaultPostForm("password", "000000") // 可设置默认值
+
+		c.JSON(http.StatusOK, gin.H{
+			"username": username,
+			"password": password,
+		})
+	})
+
+	// GET 和 POST 混合
+	router.POST("/posts", func(c *gin.Context) {
+		id := c.Query("id")
+		page := c.DefaultQuery("page", "0")
+		username := c.PostForm("username")
+		password := c.DefaultPostForm("username", "000000") // 可设置默认值
+
+		c.JSON(http.StatusOK, gin.H{
+			"id":       id,
+			"page":     page,
+			"username": username,
+			"password": password,
+		})
+	})
+
+	router.POST("/post", func(c *gin.Context) {
+		ids := c.QueryMap("ids")
+		names := c.PostFormMap("names")
+
+		c.JSON(http.StatusOK, gin.H{
+			"ids":   ids,
+			"names": names,
+		})
+	})
+
 	// Simple group: v1
 	v1 := router.Group("/v1")
 	{
 		v1.GET("/login", loginEndpoint)
-	
+
 	}
 
 	// Simple group: v2
@@ -21,7 +85,7 @@ func main() {
 
 	}
 
-	router.Run(":8080")
+	router.Run(":8090")
 }
 
 func loginEndpoint(c *gin.Context) {
